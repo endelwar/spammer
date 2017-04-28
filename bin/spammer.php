e9a8edc39e991e66b3577f5e6221d614ca129d4c
Manuel Dalla Lana <endelwar@aregar.it>
2017-04-28 02:02:27 +0200

    Join two bin script into one

--- bin/spammer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-set_time_limit(0);
-date_default_timezone_set('Europe/Rome');
-
-use EndelWar\Spammer\Application\SpammerApplication;
-
-$spammer = new SpammerApplication('Spammer', '0.8.0');
-$spammer->run();
